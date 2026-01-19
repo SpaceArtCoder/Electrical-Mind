e6790683a642bb7481dcdd0e4e35242568b25820
Ilkham <valitovilkham@gmail.com>
2026-01-19 19:55:15 +0400

    added categories component

---
+++ components/Categories/Categories.tsx
@@ -0,0 +1,11 @@
+import styles from './Categories.module.scss';
+
+export default function Categories() {
+    return (
+        <nav className={styles.nav} role='navigation' aria-label='Main menu'>
+            <ul>
+                
+            </ul>
+        </nav>
+    )
+}
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import Logo from '@/components/Logo/Logo';
 import styles from './Header.module.scss';
 
