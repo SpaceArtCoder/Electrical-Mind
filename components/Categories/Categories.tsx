3c7c5fea2fd6542151c1dfefeab3be33f5c8c18c
Ilkham <valitovilkham@gmail.com>
2026-01-19 20:21:33 +0400

    added some navigation links

--- components/Categories/Categories.tsx
+++ components/Categories/Categories.tsx
@@ -1,10 +1,16 @@
+import Link from 'next/link';
 import styles from './Categories.module.scss';
 
 export default function Categories() {
     return (
-        <nav className={styles.nav} role='navigation' aria-label='Main menu'>
+        <nav className={styles.nav} aria-label='Main navigation'>
             <ul>
-                
+                <li>
+                    <Link href='/news'>Новости</Link>
+                </li>
+                <li>
+                    <Link href='/about'>О проекте</Link>
+                </li>
             </ul>
         </nav>
     )
