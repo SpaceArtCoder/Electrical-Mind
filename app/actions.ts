e8a0b4b51831a75f7b1a217ff0dabea14c8cc5f1
Ilkham <valitovilkham@gmail.com>
2026-02-09 17:04:28 +0400

    added basic deletion logic

--- app/actions.ts
+++ app/actions.ts
@@ -3,6 +3,11 @@
 import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 
+/**
+ * 
+ * Adding an article to the DB
+ */
+
 export async function addArticleAction(formData: FormData) {
     const contentText = formData.get("content") as string;
 
@@ -24,3 +29,23 @@ export async function addArticleAction(formData: FormData) {
     revalidatePath('/'); // Refresh the data on the page
     
 }
+
+/**
+ * Deleting an article from the DB
+ */
+
+export default function deleteArticleAction(id: string) {
+
+  await prisma.article.delete({
+    where: {id},
+  });
+
+  revalidateParh('/'); // Refresh the UI to reflect the deletion
+  
+}
+
+
+
+/**
+ * Editing an existing article in the DB
+ */
