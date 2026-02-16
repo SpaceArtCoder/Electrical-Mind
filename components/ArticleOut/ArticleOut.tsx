2834914ccf9d6c7cdd7790de043483e0b49cfec6
Ilkham <valitovilkham@gmail.com>
2026-02-16 22:22:14 +0400

    imported the article store to ArticleOut

--- components/ArticleOut/ArticleOut.tsx
+++ components/ArticleOut/ArticleOut.tsx
@@ -1,6 +1,13 @@
+'use client'
+
 import styles from './ArticleOut.module.scss'
+import { useArticleStore } from '@/store/useArticleStore';
 
 export default function ArticleOut() {
+    // Get the articles from the store
+
+    const articles = useArticleStore((state) => state.articles);
+
     return (
         <div className={styles.article_out}>
             
