ec62769b28b7e03997eab19a52013f84fb703d7c
Ilkham <valitovilkham@gmail.com>
2026-02-22 21:43:32 +0400

    added article fetching functionality in CurrentNews

--- components/CurrentNews/CurrentNews.tsx
+++ components/CurrentNews/CurrentNews.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useEffect } from 'react';
 import ArticleAddForm from '../ArticleAddForm/ArticleAddForm'
 import styles from './CurrentNews.module.scss'
 import { useArticleStore } from '@/store/useArticleStore'
@@ -8,10 +9,18 @@ export default function CurrentNews() {
 
     const articles = useArticleStore((state) => state.articles);
 
+    const fetchArticles = useArticleStore((state) => state.fetchArticles);
+
+    useEffect(() => {
+        fetchArticles();
+    }, []);
+
     return (
         <section className={styles.current_news}>
             <div className={styles.main_news}>
-
+                {articles.map((article) => (
+                    <div key={article.key}>{article.content}</div>
+                ))}
             </div>
 
           { /* Test component */ }
