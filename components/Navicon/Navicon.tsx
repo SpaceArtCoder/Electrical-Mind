2d813c4370ff8b0825a94a1963488ddaff63f396
Ilkham <valitovilkham@gmail.com>
2026-03-12 21:45:38 +0400

    set a click event on a button in Navicon

--- components/Navicon/Navicon.tsx
+++ components/Navicon/Navicon.tsx
@@ -2,7 +2,7 @@ import styles from './Navicon.module.scss'
 
 export default function Navicon() {
     return (
-        <button className={styles.navicon_button}>
+        <button className={styles.navicon_button} onClick={show_categories}>
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
                 <rect y="6" width="32" height="3" rx="1.5" fill="#0a0a0a"/>
                 <rect y="14.5" width="32" height="3" rx="1.5" fill="#0a0a0a"/>
