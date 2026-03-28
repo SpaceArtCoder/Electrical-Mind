46fd2d8a833afde2636efc43779d30ee1d6ea3a3
Ilkham <valitovilkham@gmail.com>
2026-03-28 20:11:18 +0400

    connected Navicon and ui.ts

--- components/Navicon/Navicon.tsx
+++ components/Navicon/Navicon.tsx
@@ -1,8 +1,8 @@
+import type { NaviconProps } from '@/lib/types/ui'
 import styles from './Navicon.module.scss'
 
-export default function Navicon({show_nav, display}) {
+export default function Navicon({show_nav, display}: NaviconProps) {
 
-    
     return (
         <button className={!display ? styles.navicon_button : styles.hide} onClick={() => show_nav()}>
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
