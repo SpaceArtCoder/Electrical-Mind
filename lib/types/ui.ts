85f64d437bc1d45dbb2f11ddcf37c69ca2da0de7
Ilkham <valitovilkham@gmail.com>
2026-03-28 20:04:05 +0400

    defined Navicon interface in ui.ts

--- lib/types/ui.ts
+++ lib/types/ui.ts
@@ -1,3 +1,4 @@
 export interface NaviconProps {
-    
+    show_nav: () => void;           // A function that returns nothing
+    display: boolean;               // A boolean for the toggle state
 }
